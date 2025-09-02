c56fc8ab803d23ef8b1f24edeef4751a081675b8
Herclevoliet <137753824+Herclevoliet@users.noreply.github.com>
2025-09-02 04:12:12 +0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,12 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}", // ShadCN bileşenleri
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require("tailwind-animate"),
+    require("tailwind-animate") // Animasyonlar
   ],
-}
+};
